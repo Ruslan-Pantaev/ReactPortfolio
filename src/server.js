5205250eedfa1a21891e4dd593e4eaa67e06264c
Ruslan Pantaev <rpantaev@gmail.com>
2019-07-06 14:16:06 -0400

    add express.js middleware to allow cors

--- src/server.js
+++ src/server.js
@@ -40,7 +40,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(compression())
 
-// use api routes
+// allow cors for non-simple api responses
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// define api routes
 app.use('/api/codecontrol/players', players);
 app.use('/api/codecontrol/sessionsCounter', sessionsCounter);
 app.use('/api/codecontrol/sessions', sessions);
